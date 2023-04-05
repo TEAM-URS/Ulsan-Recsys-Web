50f9ffd98e0f418ce6fd35b6d7b9fe644df9f6c0
nakta-y <nakta.y.biz@gmail.com>
2023-04-05 11:36:58 +0900

    add markers & reset map range

--- front-end/react-front/src/App.js
+++ front-end/react-front/src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
-import ShowMap from './kakao_api/ShowKakaoMap';
+import ShowMap, { cordinatesToMarker, saveCoordinatesToAddress } from './kakao_api/KakaoMap';
 import Modal from './modal/surveyModal';
 import Backdrop from './background/backDrop';
+
+let addresses = ['울산 중구 태화동 107', '울산 동구 일산동 905', '울산 울주군 서생면 진하리 307-2', '울산 동구 일산동 905-7']
 
 const App = () => {
   const [showModal, setShowModal] = useState(true);
   const [showBackdrop, setShowBackdrop] = useState(true);
 
   const closeModalHandler = () => {
+
+    saveCoordinatesToAddress(addresses).then(() => {
+      cordinatesToMarker();
+    });
+
     setShowBackdrop(false);
     setShowModal(false);
   };
---
+++ front-end/react-front/src/kakao_api/KakaoMap.js
@@ -0,0 +1,85 @@
+import React, { useEffect } from 'react'
+
+const kakao = window.kakao;
+let map = null; 
+let places = [];
+
+const ShowMap=()=>{
+
+  useEffect(()=>{
+    var container = document.getElementById('map');
+    var options = {
+      center: new kakao.maps.LatLng(35.5383773, 129.3113596),
+      level: 6
+    };
+
+    map = new kakao.maps.Map(container, options);
+  });
+
+    return (
+        <div style={{ width: '100%', height: '100vh' }}>
+            <div id="map" style={{ width: '100%', height: '100%' }}></div>
+        </div>
+    )
+}
+
+export function saveCoordinatesToAddress(addresses) {
+  return new Promise(function(resolve, reject) {
+    places = [];
+    var geocoder = new kakao.maps.services.Geocoder();
+
+    addresses.forEach((address, index) => {
+      geocoder.addressSearch(address, function(result, status) {
+        if (status === kakao.maps.services.Status.OK) {
+
+          places.push(new kakao.maps.LatLng(result[0].y, result[0].x));
+
+          // 모든 주소 변환 작업이 완료되면 resolve()를 실행합니다.
+          if (index === addresses.length - 1) {
+            resolve();
+          }
+        } else {
+          reject(new Error('주소 변환 실패'));
+        }
+      });
+    });
+  });   
+}
+
+export function cordinatesToMarker() {
+
+  setTimeout(function() {
+    // 1초 후 실행될 코드
+    // 마커 이미지의 이미지 주소입니다
+    var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+
+    // 지도를 재설정할 범위정보를 가지고 있을 LatLngBounds 객체를 생성합니다
+    var bounds = new kakao.maps.LatLngBounds();
+
+    for (var i = 0; i < places.length; i ++) {
+
+      var marker = null;
+
+      // 마커 이미지의 이미지 크기 입니다
+      var imageSize = new kakao.maps.Size(24, 35); 
+      
+      // 마커 이미지를 생성합니다    
+      var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+
+      // 마커를 생성합니다
+      marker = new kakao.maps.Marker({
+          map: map, // 마커를 표시할 지도
+          position: places[i], // 마커를 표시할 위치
+          image : markerImage // 마커 이미지 
+      });
+      marker.setMap(map);
+
+      // LatLngBounds 객체에 좌표를 추가합니다
+      bounds.extend(places[i]); 
+    }
+
+    map.setBounds(bounds);
+  }, 1000);
+}
+
+export default ShowMap;
