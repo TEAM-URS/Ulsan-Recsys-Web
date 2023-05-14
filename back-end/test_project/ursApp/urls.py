ce141ea2e58bb7ef572d07bbd90a36e9e4f3d9c8
ChangZero <kimcy9973@gmail.com>
2023-05-14 19:40:23 +0900

    명소추천추가 & 신규가입자 사전설문페이지 생성

--- back-end/test_project/ursApp/urls.py
+++ back-end/test_project/ursApp/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('recsys/', views.recsys, name='recsys'),
-    path('kakaomap/', views.kakaomap, name='kakaomap')
+    path('recsys_a/', views.recsys_a, name='recsys_a'),
+    path('recsys_r/', views.recsys_r, name='recsys_r'),
+    path('kakaomap/', views.kakaomap, name='kakaomap'),
+    path('questionnaire/', views.questionnaire, name='questionnaire')
 ]
 
